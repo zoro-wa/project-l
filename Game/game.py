277a99432233f814e22d318f8adefbabfc226263
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-08-27 13:04:14 +0545

    Added Live-score on the game-display

--- Game/game.py
+++ Game/game.py
@@ -1,6 +1,11 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surface = test_font.render(f'Score : {current_time}',False,(64,64,64))
+    score_rect = score_surface.get_rect(center = (400,50))
+    screen.blit(score_surface,score_rect)
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -8,12 +13,13 @@ pygame.display.set_caption("Marimo")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("Font/Pixeltype.ttf", 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load("Graphics/sky.png").convert()
 ground_surface = pygame.image.load("Graphics/ground.png").convert()
 
-score_surface = test_font.render("Marimo", False, (64, 64, 64))
-score_rect = score_surface.get_rect(center=(400, 50))
+#score_surface = test_font.render("Marimo", False, (64, 64, 64))
+#score_rect = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load("Graphics/Snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300))
@@ -39,13 +45,16 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
+                snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, "#c0ebec", score_rect)
-        pygame.draw.rect(screen, "#c0ebec", score_rect, 10)
-        screen.blit(score_surface, score_rect)
+        #pygame.draw.rect(screen, "#c0ebec", score_rect)
+        #pygame.draw.rect(screen, "#c0ebec", score_rect, 10)
+        #screen.blit(score_surface, score_rect)
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0:

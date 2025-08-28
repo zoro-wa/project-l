a361e45ef8dfdede842d0134b76f44830db502f1
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-08-28 22:30:07 +0545

    Added jump music and bg-music, game is complete

--- Game/game.py
+++ Game/game.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 from random import randint, choice
-
+ 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -14,11 +14,14 @@ class Player(pygame.sprite.Sprite):
         self.image = self.player_walk[self.player_index]
         self.rect = self.image.get_rect(midbottom = (200,300))
         self.gravity = 0
-
+        
+        self.jump_sound = pygame.mixer.Sound('Audio/jump.mp3')
+        self.jump_sound.set_volume(0.3)
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
           self.gravity = -20
+          self.jump_sound.play()
     
     def apply_gravity(self):
         self.gravity += 1
@@ -140,6 +143,9 @@ test_font = pygame.font.Font("Font/Pixeltype.ttf", 50)
 game_active = False
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('Audio/music.wav')
+bg_music.play(loops = -1)
+bg_music.set_volume(0.1)
 
 player = pygame.sprite.GroupSingle()
 player.add(Player())
